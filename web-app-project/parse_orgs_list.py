91499175687e1bac0a248572c29ab403bd6d5186
Andrea Llamas <andreaillamas@yahoo.com>
2020-07-23 15:26:51 -0400

    Update parse_orgs_list

--- web-app-project/parse_orgs_list.py
+++ web-app-project/parse_orgs_list.py
@@ -1,6 +1,6 @@
 
 import pandas as pd
 
-df = pd.read_csv('web-app-project\orgs_list.csv', encoding = 'utf-8')
-rows = df.apply(lambda x: x.tolist(), axis = 1) 
+df = pd.read_csv('web-app-project\orgs_list.csv', index_col=0)
+rows = df.apply(lambda x: x.tolist()) 
 print(rows)
