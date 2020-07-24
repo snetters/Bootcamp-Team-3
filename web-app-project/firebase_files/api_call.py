f9ddfc774914f4cb00ea033ee2910769019814e0
Andrea Llamas <andreaillamas@yahoo.com>
2020-07-23 22:13:30 -0400

    Return message if org not in database

--- web-app-project/firebase_files/api_call.py
+++ web-app-project/firebase_files/api_call.py
@@ -6,12 +6,17 @@ import ast
 
 app = Flask(__name__)
 
-query = "black transmen inc" #would come from requests.args
+query = "black transmens inc" #would come from requests.args
+org_list = get_orgs.org_list()
+if query in org_list:
+    org_index = org_list.index(query)
+    ein = get_orgs.ein_list()[org_index]
 
-org_index = get_orgs.org_list().index(query)
-ein = get_orgs.ein_list()[org_index]
+    search = "https://projects.propublica.org/nonprofits/api/v2/organizations/" + ein + ".json"
+    response = requests.get(search)
+    print(response.content)
 
-search = "https://projects.propublica.org/nonprofits/api/v2/organizations/" + ein + ".json"
-response = requests.get(search)
-print(response.content)
+else:
+    response = "We do not have " + query + " in our database \nSorry about that!"
+    print(response)
 
