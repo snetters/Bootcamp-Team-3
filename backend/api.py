0ad2e8674423fed3c1bd28ef84025e31e146b08f
Amaya Howard-Carswell <akh2161@columbia.edu>
2020-07-23 22:30:44 -0400

    Add 3.7 py venv

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,36 @@
 from flask import Flask
 from flask import request
+import pyodbc
+import requests
+
 
 import ast
 
 app = Flask(__name__)
+
+##conn = pyodbc.connect('Driver={SQL Server};'
+##                      'Server=DESKTOP-TB9FI1L\SQLEXPRESS;'
+##                      'Database=FFDB;'
+##                      'Trusted_Connection=yes;')
+server = 'fundflow-server.database.windows.net'
+database = 'FFDB'
+username = 'akh2161@columbia.edu@fundflow-server'
+password = 'IBMblue2020#'   
+driver= '{ODBC Driver 17 for SQL Server}'
+
+conn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = conn.cursor()
+# Within a function query = request.args
+query = 'National Urban League'
+q = cursor.execute('SELECT ein FROM FFDB.dbo.Companies WHERE name = ?', query.title()).fetchall()[0][0] # .title() ensures every word starts with a capital letter to match the table entries
+print(q)
+# Earlier testing since cursor returns a list of results
+#for row in cursor:
+#    print(row[0])
+response = requests.get("https://projects.propublica.org/nonprofits/api/v2/organizations/{}.json".format(q)) 
+# Still need to get front end to actually display tax info from the content recieved 
+print(response.content["filings_with_data"])
+
 
 
 @app.route("/backend/time")
@@ -74,6 +101,16 @@ def userInfoVerification(inputParams):
         return {"error": "passIsUpper"}
 
     ## We can put the username through the database here to check if this user already exists
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM FFDB.dbo.Users WHERE username = ' + username + 'OR email = ' + email)
+
+    if cursor == null:
+        cursor.execute('INSERT INTO FFDB.dbo.Users (username, password, email) VALUES ('+username+', '+pasW+','+email+')')
+    else:
+        return {"error"} #Specify error that username or email are already in use
+
+
     ## At this point, the user information is valid
 
     ## We send this data to the database
@@ -102,11 +139,42 @@ def loginVerification(inputParams):
     pasW = inputParams.get("pass")
 
     ## VERIFY W/ DATABASE HERE
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM FFDB.dbo.Users WHERE username = {} AND password = {}').format(username, pasW)
+    #cursor.execute('SELECT * FROM FFDB.Users WHERE username = ' + username + 'AND password = ' + pasW)
+
+
+    if cursor != null:
+        return {} # login successful
+    else:
+        return {"error"} #Specify error that username or password are not correct
 
     ## We can put the username through the database here to check if this user already exists
     ## At this point, the user information is valid
+
+
 
     ## We send this data to the database
     userInfo = {"username": username, "pass": pasW, "verified": True}
 
     return userInfo
+
+@app.route('/backend/search', methods=['GET', 'POST'])
+def search_data():
+
+    # First clears out the old search items
+    # 
+    # Gets value of term user sent in
+    if request.method == "POST":
+        json_data = request.get_json()
+        search_term = json_data["search_term"]
+
+    #for item in products:
+    #    if search_term.lower() in item["name"].lower():
+    #        search_match.append(item)
+
+    ## send back the WHOLE array of data, so the client can redisplay it
+    #return jsonify(products=products, search_match=search_match, size=search_size)
+
+    # Just for testing purposes right now
+    return true;
