84de35851006f0a9fc9bf277b80cd8bf859ef818
Amaya Howard-Carswell <akh2161@columbia.edu>
2020-07-28 15:51:11 -0400

    Fixed login and create account functions in accounts, small comments in donations.

--- web-app-project/accounts.py
+++ web-app-project/accounts.py
@@ -1,11 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template, jsonify
 from firebase import firebase
 
 firebase = firebase.FirebaseApplication("https://fundflow-team3.firebaseio.com/", None)
 result = firebase.get("/fundflow-team3/Users", '')
 
+
 def user_info():
     return result
+
 
 def get_user_list():
     user_list = []
@@ -13,65 +15,66 @@ def get_user_list():
         user_list.append(key["username"])
     return user_list
 
-<<<<<<< HEAD
+
+def get_email_list():
+    email_list = []
+    for key in result:
+        email_list.append(key["email"])
+    return email_list
+
 # Can change the route name
 @app.route('/backend/create_account', methods=['GET', 'POST'])
-=======
->>>>>>> 048e1e82fb2846b9ac7f0bc86776baecb5152d71
 def create_account():
     # Assuming the method is post
     # This gets the data sent over from an ajax call (if that's what's being used)
     data = request.get_json()
+    user_list = get_user_list()
+    email_list = get_email_list()
+
     # Dictionary values aren't set in stone. Need to change it to what values are sent over from frontend
     if data["password"] != data["confirm_password"]:
-<<<<<<< HEAD
-        print("Passwords do not match. Please retype")
-        return "Passwords do not match. Please retype" #not sure what it should actually return, depends on front end
-    else:
-        user_list = get_user_list()
-        if data["username"] in user_list:
-            print("Username is taken. Please enter a different username")
-            return "Username is taken. Please enter a different username" #same as password
-=======
-        # This is just an example of what could be sent back with jsonify
-        # Depends on what the backend is
         response = {
-            "error": "The password do not match.",
+            "error": "The passwords do not match.",
+            "success": False
+        }
+        # Could be render template depending on the front end
+        return jsonify(response=response)
+    # The username is already present in the DB
+    elif data["username"] in user_list:
+        response = {
+            "error": "The username is already in use. Please choose a new one.",
+            "success": False
+        }
+        # Could be render template depending on the front end
+        return jsonify(response=response)
+
+    elif data["email"] in email_list:
+        response = {
+            "error": "The email is already in use. Please enter a new one.",
             "success": False
         }
         return jsonify(response=response)
     else:
-        user_list = get_user_list()
-        if data["username"] in user_list:
-            response = {
-                "error": "This username is taken. Please enter a new one.",
-                "success": False
-            }
-            return jsonify(response=response)
->>>>>>> 048e1e82fb2846b9ac7f0bc86776baecb5152d71
-        else:
-            data = {
-            'name': name,
-            'username': username,
-            'password': password,
-            'email': email,
-            }
-            result = firebase.post('/fundflow-team3/Users', data)
+        data = {
+            'name': data["name"],
+            'username': data["username"],
+            'password': data["password"],
+            'email': data["email"]
+        }
+        res = firebase.post('/fundflow-team3/Users', data)
 
-<<<<<<< HEAD
-=======
-            donations_set_up = {
-                'name': name, 
-                'username': username, 
-                'email': email,
-                'org_list': list([["None", 0]])
-                }
+        donations_set_up = {
+            'name': data["name"],
+            'username':  data["username"],
+            'email': data["email"],
+            'org_list': list([["None", 0]])
+        }
 
-            donations_result = firebase.post('/fundflow-team3/past_donations', donations_set_up)
-    # Based on a route to a dashboard that is specific to the user
-    return redirect("/dashboard/" + data["username"])
+        donations_result = firebase.post('/fundflow-team3/past_donations', donations_set_up)
+        # Based on a route to a dashboard that is specific to the user
+        return render_template("dashboard.html", user_info=data["username"])
 
->>>>>>> 048e1e82fb2846b9ac7f0bc86776baecb5152d71
+
 @app.route('/backend/login', methods=['GET', 'POST'])
 def login():
     # Assuming the method is post
@@ -79,36 +82,22 @@ def login():
     data = request.get_json()
     user_list = get_user_list()
 
-<<<<<<< HEAD
-    # Would you want to check for email as well
-=======
-    # Would you want to check for email as well? #we can do it as a backup if they forget username
->>>>>>> 048e1e82fb2846b9ac7f0bc86776baecb5152d71
     if data["username"] not in user_list:
         response = {
             "error": "We do not have an account registered under that username",
             "success": False
         }
         return jsonify(response=response)
-    else:
+    elif data["password"] == full_dict[data["username"]]:
+        return render_template("dashboard.html", user=data["username"])
         # Does full_dict check everything at once? #full dict gets all the users in our database
-        full_dict = user_info()
-        if password == full_dict[data["username"]]:
-<<<<<<< HEAD
-            return redirect("/dashboard") # Or whatever we call the user homepage
-=======
-            return redirect("/dashboard/" + data["username"]) 
->>>>>>> 048e1e82fb2846b9ac7f0bc86776baecb5152d71
-        else:
-            response = {
-                "error": "The password does not match the password in the system",
-                "success": False
-            }
-            return jsonify(response=response)
-<<<<<<< HEAD
-=======
+    else:
+        response = {
+            "error": "The password does not match the password in the system",
+            "success": False
+        }
+        return jsonify(response=response)
 
->>>>>>> 048e1e82fb2846b9ac7f0bc86776baecb5152d71
 
 # test create_account("Andrea", "andi", "andi101", "andi101", "andrea@gmail.com")
 
--- web-app-project/firebase_files/update_donations.py
+++ web-app-project/firebase_files/update_donations.py
@@ -14,6 +14,7 @@ username = "test_user"
 amount = 100
 org = "NAACP"
 
+
 def get_donations(username):
     for key in result:
         if result[key]["username"] == username:
@@ -21,20 +22,17 @@ def get_donations(username):
         #user has to already be in our database
 
 
-
 def update_donations(amount, org):
-    #data = requests.json()
+    # data = requests.json()
     key = get_donations(username)
     previous_donations = result[key]["org_list"]
     if len(previous_donations) == 1 and previous_donations[0][0] == "None":
-       first_donation = list([[org, amount]])
-       update_dict = firebase.put("/fundflow-team3/past_donations/" + key, "org_list", first_donation )
+        first_donation = list([[org, amount]])
+        update_dict = firebase.put("/fundflow-team3/past_donations/" + key, "org_list", first_donation)
     else:
         new_donation = previous_donations.append([org, amount])
-        update_dict = firebase.put("/fundflow-team3/past_donations/" + key, "org_list", new_donation )
+        update_dict = firebase.put("/fundflow-team3/past_donations/" + key, "org_list", new_donation)
 
 
-
-
-final = update_donations(amount, org)
-print(final)
+# final = update_donations(amount, org)
+# print(final)
